84b8995f1eb8cece65a9e707cfbd64ad4103184b
firazura <guest@localhost.localdomain>
2019-04-11 19:12:54 +0700

    add delivery order client side

--- booking_mobil/booking_mobil/doctype/delivery_order/delivery_order.js
+++ booking_mobil/booking_mobil/doctype/delivery_order/delivery_order.js
@@ -6,17 +6,17 @@ frappe.ui.form.on('Delivery Order', {
 
 	},
 	// handle delivery order line table
-	nama_customer: function(frm){
-		if(frm.doc.nama_customer){
+	id_customer: function(frm){
+		if(frm.doc.id_customer){
 			frappe.call({
 				method: "frappe.client.get",
 				args:{
 					doctype: "Sales Order Mobil",
-					name: frm.doc.nama_customer
+					name: frm.doc.id_customer
 				},
 				callback: function(r){
 					if(r.message.sales_mobil_line[0]){
-						frappe.model.add_child(cur_frm.doc, "Deliver Order Line", "delivery_order_line");
+						frappe.model.add_child(cur_frm.doc, "Delivery Order Line", "delivery_order_line");
 					$.each(frm.doc.delivery_order_line || [], function(i, v){
 						var data_sales = r.message.sales_mobil_line[0];
 							frappe.model.set_value(v.doctype, v.name, "kode_mobil",
@@ -31,11 +31,31 @@ frappe.ui.form.on('Delivery Order', {
 				}
 			})
 		}
+	},
+	// check if the delivery date is before today's date or after tanggal sewa
+	tanggal_deliver:function(frm){
+		frappe.call({
+			method: "frappe.client.get",
+			args:{
+				doctype: "Sales Order Mobil",
+				name: frm.doc.id_customer
+			},
+			callback: function(r){
+				var tanggalSewa = r.message.tanggal_sewa;
+				if(frm.doc.tanggal_deliver > tanggalSewa){
+					frm.set_value('tanggal_deliver', '');
+					frappe.throw(__('Tanggal deliver tidak bisa setelah tanggal sewa'));
+				}else if(frm.doc.tanggal_deliver < get_today()){
+					frm.set_value('tanggal_deliver', '');
+					frappe.throw(__('Tidak dapat memilih tanggal terlebih dahulu'));
+				}
+			}
+		})
 	}
 });
 
 // Filter only submitted data
-cur_frm.set_query('nama_customer', function(){
+cur_frm.set_query('id_customer', function(){
 	return{
 		filters:[
 			['Sales Order Mobil', 'docstatus', '=', '1']
