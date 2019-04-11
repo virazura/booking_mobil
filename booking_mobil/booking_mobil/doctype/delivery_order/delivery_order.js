284585b875e38aebe5a4825aed62d071a3bbaa0f
firazura <guest@localhost.localdomain>
2019-04-11 14:52:44 +0700

    add client-side for delivery-order

--- booking_mobil/booking_mobil/doctype/delivery_order/delivery_order.js
+++ booking_mobil/booking_mobil/doctype/delivery_order/delivery_order.js
@@ -4,5 +4,41 @@
 frappe.ui.form.on('Delivery Order', {
 	refresh: function(frm) {
 
+	},
+	// handle delivery order line table
+	nama_customer: function(frm){
+		if(frm.doc.nama_customer){
+			frappe.call({
+				method: "frappe.client.get",
+				args:{
+					doctype: "Sales Order Mobil",
+					name: frm.doc.nama_customer
+				},
+				callback: function(r){
+					if(r.message.sales_mobil_line[0]){
+						frappe.model.add_child(cur_frm.doc, "Deliver Order Line", "delivery_order_line");
+					$.each(frm.doc.delivery_order_line || [], function(i, v){
+						var data_sales = r.message.sales_mobil_line[0];
+							frappe.model.set_value(v.doctype, v.name, "kode_mobil",
+							data_sales.kode_mobil);
+							frappe.model.set_value(v.doctype, v.name, "nama_mobil", data_sales.nama_mobil);
+							frappe.model.set_value(v.doctype, v.name, "no_polisi", data_sales.no_polisi);
+							frappe.model.set_value(v.doctype, v.name, "harga_mobil", data_sales.harga_mobil);
+							})
+					}
+					
+					frm.refresh_field("delivery_order_line");
+				}
+			})
+		}
 	}
 });
+
+// Filter only submitted data
+cur_frm.set_query('nama_customer', function(){
+	return{
+		filters:[
+			['Sales Order Mobil', 'docstatus', '=', '1']
+		]
+	}
+})
